7fba28fe8712c4bc371d67fc9d2da5abce77fc71
XavierTwitty <x.twitty@gmail.com>
2021-02-21 21:52:43 -0500

    useForm, working on test

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,12 @@
 // write your custom hook here to control your checkout form
+
+import { useState } from "react";
+
+export const useForm = (key, initialValues) => {
+  const [value, setValue] = useState(key, initialValues);
+
+  const handleChange = (value) => {
+    setValue(value);
+  };
+  return [value, handleChange];
+};
