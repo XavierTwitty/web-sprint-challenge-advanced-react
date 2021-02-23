ae253e59bb9dbf93cd6c55da55db26c89ab1db5f
XavierTwitty <x.twitty@gmail.com>
2021-02-22 20:16:10 -0500

    all test are passing

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from "@testing-library/user-event";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
@@ -8,7 +9,18 @@ test("form header renders", () => {
   render(<CheckoutForm />);
 });
 
-test("form shows success message on submit with form details", () => {
+test("form shows success message on submit with form details", async () => {
   render(<CheckoutForm />);
-  const firstNameInput = screen.getby;
+  const firstNameInput = screen.getByLabelText(/first name/i);
+  const lastNameInput = screen.getByLabelText(/last name/i);
+  const addressInput = screen.getByLabelText(/address/i);
+  const cityInput = screen.getByLabelText(/city/i);
+  const stateInput = screen.getByLabelText(/state/i);
+  const zipInput = screen.getByLabelText(/zip/i);
+  userEvent.type(firstNameInput, "Xavier");
+  userEvent.type(lastNameInput, "Twitty");
+  userEvent.type(addressInput, "304 follow rd");
+  userEvent.type(cityInput, "shelby");
+  userEvent.type(stateInput, "NC");
+  userEvent.type(zipInput, "32333");
 });
